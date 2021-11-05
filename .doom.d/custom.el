bb64ed6879685e7204ee0330a45e4438cc6f70e2
spassos <spassos98@gmail.com>
2021-11-05 14:22:45 -0500

    Added doom emacs configuration

---
+++ .doom.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/home/spassos/org/org-basics.org" "/home/spassos/org/pendingLinux.org" "/home/spassos/org/studyRoad.org" "/home/spassos/org/test.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
